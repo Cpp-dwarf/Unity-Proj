02497920468a5f270e1ca9e9abaa19de389fa28d
raudry <50342144+raudry@users.noreply.github.com>
2020-04-03 22:38:31 -0500

    Add files via upload

---
+++ Stats/Stat.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Stat
+{
+    [SerializeField]
+    private int baseVal;
+    
+    public int GetVal ()
+    {
+        return baseVal;
+    }
+}
