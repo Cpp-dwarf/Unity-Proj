d46561222c8e38f118d68336e16ec463cdd0bb78
cpp-dwarf <sagess@outlook.com>
2020-04-10 12:27:59 -0500

    update, began combat scripts

---
+++ Assets/EnemyAIhandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAIhandler : MonoBehaviour
+{
+
+    [SerializeField] float m_speed = 1.0f;
+    [SerializeField] float m_jumpForce = 2.0f;
+
+    private Animator m_animator;
+    private Rigidbody2D m_body2d;
+    private Sensor_Bandit m_groundSensor;
+    private bool m_grounded = false;
+    private bool m_combatIdle = false;
+    private bool m_isDead = false;
+
+    public int maxHealth = 100;
+    public int currentHealth;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+        m_body2d = GetComponent<Rigidbody2D>();
+        m_groundSensor = transform.Find("GroundSensor").GetComponent<Sensor_Bandit>();
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void takeDamage(int damage)
+    {
+        currentHealth -= damage;
+        animator.SetTrigger("Hurt");
+        if(currentHealth==0)
+        {
+            Die();
+        }
+    }
+    public void Die()
+    {
+
+    }
+}
