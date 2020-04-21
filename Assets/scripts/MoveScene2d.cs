913412174e84aa9c26be0347695cc21dc64a5a63
Elibaby <egwincorporated@gmail.com>
2020-04-21 13:25:11 -0500

    Merge
    Merge

---
+++ Assets/scripts/MoveScene2d.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveScene2d : MonoBehaviour
+{
+    public string triggerName;
+    public string levelToLoad;
+    public Transform spawnPoint;
+    public bool enable = true; //Enable is so that if the door behind the bookshelf does not look for the player while the bookshelf is in front of it
+
+    void OnTriggerEnter2D(Collider2D x)
+    {
+        if (x.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
+        {
+            if (enable)
+            {
+                SceneLoader.instance.OnEnteredExitTrigger(triggerName, levelToLoad);
+                enable = false;
+            }
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D x)
+    {
+        if (x.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
+        {
+            if (enable)
+            {
+                SceneLoader.instance.OnEnteredExitTrigger(triggerName, levelToLoad);
+                enable = false;
+            }
+        }
+    }
+}
+
